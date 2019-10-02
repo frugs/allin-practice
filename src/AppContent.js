6b5d3263c3f9f4abebaf2233106a760b96adadea
Hugo Wainwright <hugowainwright@fb.com>
2019-10-02 15:07:44 +0100

    fix for tab bar

--- src/AppContent.js
+++ src/AppContent.js
@@ -6,10 +6,28 @@ function AppContent(props) {
     return (
         <Tabs large selectedTabId="profile">
             <span className="ml-2"/>
-            <Tab id="profile" icon="person"
-                 title={<><Icon icon="person" className="mx-2"/>Profile</>} panel={<ProfilePanel {...props}/>}/>
-            <Tab id="search" icon="search" title={<><Icon icon="search" className="mx-2"/>Search</>}/>
-            <Tab id="explore" icon="explore" title={<><Icon icon="globe-network" className="mx-2"/>Explore</>}/>
+            <Tab
+                id="profile"
+                icon="person"
+                title={
+                    <><Icon icon="person" className="ml-2 mr-1"/><span className="ml-1 mr-2">Profile</span></>
+                }
+                panel={<ProfilePanel {...props}/>}
+            />
+            <Tab
+                id="search"
+                icon="search"
+                title={
+                    <><Icon icon="search" className="ml-2 mr-1"/><span className="ml-1 mr-2">Search</span></>
+                }
+            />
+            <Tab
+                id="explore"
+                icon="explore"
+                title={
+                    <><Icon icon="globe-network" className="ml-2 mr-1"/><span className="ml-1 mr-2">Explore</span></>
+                }
+            />
         </Tabs>
     );
 }
